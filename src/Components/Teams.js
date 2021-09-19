a28fc27d2f7bfc01d980ccc6e061f97f59543546
nisha2204 <modaninisha@gmail.com>
2021-09-19 22:00:15 +0530

    ---

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -213,7 +213,7 @@ const Teams = () => {
                     <h5 className="project-name" style={{color:`${col}`,backgroundColor:`${col}22`, width:`${len(team.project)}`}}>{team.project}</h5>
                     <div className="options">
 
-                      <div className="edit">
+                      <div className="edit" >
 
                       <Link to={`/editTeam/${team._id}`} className="btn btn-success"> <svg
                   className="editOption"
@@ -236,13 +236,9 @@ const Teams = () => {
                   </defs>
                 </svg></Link>&nbsp;
                       </div>
-                      <div className="edit">
-              <Link to={`/addMember/${team._id}`} className="btn btn-success"> <svg
-                  className="editOption"
-
+                      <div className="edit" >
               <Link to={`/addMember/${team._id}`} className="btn btn-success">
               <svg className="editOption"
-
                   width="24"
                   height="24"
                   viewBox="0 0 24 24"
@@ -297,33 +293,7 @@ const Teams = () => {
                 </div>
               </div>
               
-              //   <div classNameName="team" >
-              //     {console.log(team._id)}
-              //   <h4 classNameName="team-name">{team.Name}</h4>
-              //   <h5 classNameName="project-name">{team.project}</h5>
-              //   <p classNameName="project-description">{team.description}</p>
-              //   <div classNameName="team-members">
-              //     <h5 classNameName="total-members">
-            //       Total Members <span classNameName="number">{team.members}</span>
-            //     </h5>
-            //     <div classNameName="add-btn"
-            //     >
-            //       <Link style={{textDecoration: 'none'}} to='/taskinteam'>
-            //       <svg
-            //         width="24"
-            //         height="24"
-            //         viewBox="0 0 24 24"
-            //         fill="none"
-            //         xmlns="http://www.w3.org/2000/svg"
-            //       >
-            //         <path
-            //           d="M12 0C5.373 0 0 5.373 0 12C0 18.627 5.373 24 12 24C18.627 24 24 18.627 24 12C24 5.373 18.627 0 12 0ZM18 13H13V18H11V13H6V11H11V6H13V11H18V13Z"
-            //           fill="#BAC8DF"
-            //         />
-            //       </svg></Link>
-            //     </div>
-            //   </div>
-            // </div>
+              
             ))}
             </div>
       </div>)
@@ -335,181 +305,3 @@ export default Teams
 
 
 
-/*<div classNameName="list" style={{display:'none'}} id={team.listId}>
-                  {team.list.map((item,{col = randomColor()}) => (
-                <div classNameName="item">
-                  <div classNameName="top-section">
-                    <h5 classNameName="tagline" style={{color:`${col}`,backgroundColor:`${col}33`, width:`${len(item.tagline)}`}}>{item.tagline}</h5>
-                    <div classNameName="options">
-            <div classNameName="edit">
-              <svg
-                classNameName="editOption"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g clipPath="url(#clip0)">
-                  <path
-                    d="M19.769 9.923L7.127 22.562L0 24L1.438 16.872L14.079 4.232L19.769 9.923V9.923ZM21.183 8.509L24 5.689L18.309 0L15.493 2.817L21.183 8.509V8.509Z"
-                    fill="#bac8df"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0">
-                    <rect width="24" height="24" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </div>
-            <div classNameName="completed">
-              <svg
-                classNameName="completedOption"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                >
-                <path
-                  onClick= { markAsComplete }
-                  classNameName="completedOptionPath"
-                  d="M12 0C5.373 0 0 5.373 0 12C0 18.627 5.373 24 12 24C18.627 24 24 18.627 24 12C24 5.373 18.627 0 12 0ZM10.75 17.292L6.25 12.928L8.107 11.07L10.75 13.576L16.393 7.792L18.25 9.649L10.75 17.292Z"
-                />
-              </svg>
-            </div>
-            <div classNameName="delete">
-              <svg
-              onClick = {deleteTask}
-                classNameName="deleteOption"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M12 0C5.373 0 0 5.373 0 12C0 18.627 5.373 24 12 24C18.627 24 24 18.627 24 12C24 5.373 18.627 0 12 0ZM16.151 17.943L12.008 13.841L7.891 18L6.058 16.167L10.162 12.01L6 7.891L7.833 6.058L11.988 10.16L16.094 6L17.943 7.849L13.843 11.99L18 16.094L16.151 17.943Z"
-                  fill="#FF3A3A"
-                />
-              </svg>
-            </div>
-          </div>
-                  </div>
-                  <h5 classNameName="description">
-                    {item.description}
-                  </h5>
-                  <div classNameName="bottom-section">
-                    <div classNameName="duration">
-                      <svg
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M4 24H2V0H4V24ZM22 2.613C22 2.613 20.379 4.043 18.246 4.043C14.886 4.043 14.81 1.148 10.909 1.148C8.801 1.148 6.834 2.128 6 2.842V14.927C7.184 14.108 8.979 13.246 10.923 13.246C14.607 13.246 15.124 16 18.407 16C20.529 16 22 14.641 22 14.641V2.613V2.613Z"
-                          fill="#BAC8DF"
-                        />
-                      </svg>
-                      <h5 classNameName="date">{item.date}</h5>
-                    </div>
-                    <div classNameName="profile">
-                      <h5 classNameName="name">{item.name}</h5>
-                      <p classNameName="domain">{item.domain}</p>
-                    </div>
-                  </div>
-                </div>
-                  ))}
-                
-              </div>
-              <div classNameName="drop-down-btn">*/
-/*
- {team.list.map((item,{col = randomColor()}) => (
-                  <div classNameName="item">
-                    <div classNameName="top-section">
-                      <h5 classNameName="tagline" style={{color:`${col}`,backgroundColor:`${col}33`, width:`${len(item.tagline)}`}}>{item.tagline}</h5>
-                      <div classNameName="options">
-              <div classNameName="edit">
-                <svg
-                  classNameName="editOption"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g clipPath="url(#clip0)">
-                    <path
-                      d="M19.769 9.923L7.127 22.562L0 24L1.438 16.872L14.079 4.232L19.769 9.923V9.923ZM21.183 8.509L24 5.689L18.309 0L15.493 2.817L21.183 8.509V8.509Z"
-                      fill="#bac8df"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0">
-                      <rect width="24" height="24" fill="white" />
-                    </clipPath>
-                  </defs>
-                </svg>
-              </div>
-              <div classNameName="completed">
-                <svg
-                  classNameName="completedOption"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                  >
-                  <path
-                    onClick= { markAsComplete }
-                    classNameName="completedOptionPath"
-                    d="M12 0C5.373 0 0 5.373 0 12C0 18.627 5.373 24 12 24C18.627 24 24 18.627 24 12C24 5.373 18.627 0 12 0ZM10.75 17.292L6.25 12.928L8.107 11.07L10.75 13.576L16.393 7.792L18.25 9.649L10.75 17.292Z"
-                  />
-                </svg>
-              </div>
-              <div classNameName="delete">
-                <svg
-                onClick = {deleteTask}
-                  classNameName="deleteOption"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M12 0C5.373 0 0 5.373 0 12C0 18.627 5.373 24 12 24C18.627 24 24 18.627 24 12C24 5.373 18.627 0 12 0ZM16.151 17.943L12.008 13.841L7.891 18L6.058 16.167L10.162 12.01L6 7.891L7.833 6.058L11.988 10.16L16.094 6L17.943 7.849L13.843 11.99L18 16.094L16.151 17.943Z"
-                    fill="#FF3A3A"
-                  />
-                </svg>
-              </div>
-            </div>
-                    </div>
-                    <h5 classNameName="description">
-                      
-                    </h5>
-                    <div classNameName="bottom-section">
-                      <div classNameName="duration">
-                        <svg
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path
-                            d="M4 24H2V0H4V24ZM22 2.613C22 2.613 20.379 4.043 18.246 4.043C14.886 4.043 14.81 1.148 10.909 1.148C8.801 1.148 6.834 2.128 6 2.842V14.927C7.184 14.108 8.979 13.246 10.923 13.246C14.607 13.246 15.124 16 18.407 16C20.529 16 22 14.641 22 14.641V2.613V2.613Z"
-                            fill="#BAC8DF"
-                          />
-                        </svg>
-                        
-                      </div>
-                      <div classNameName="profile">
-                        
-                      </div>
-                    </div>
-                  </div>
-                    ))}*/
